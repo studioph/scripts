955d031e06dc5b904a791c5e70c67e886ca906d1
Paul <dev@studiop.page>
2021-07-26 20:52:51 -0400

    Updated Pop script

--- pop-setup.sh
+++ pop-setup.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
-###########################
-# WRITTEN FOR POP!_OS 20.04 (nvidia_13) (v440.100)
-###########################
+##############################
+# WRITTEN FOR POP!_OS 20.04.2
+##############################
 
 # Prevent Nvidia drivers from being upgraded
 sudo apt-mark hold \
-	xserver-xorg-video-nvidia-440 \
+	xserver-xorg-video-nvidia-465 \
+	libnvidia-cfg1-465 \
+	libnvidia-common-465 \
+	libnvidia-compute-465 \
+	libnvidia-decode-465 \
+	libnvidia-encode-465 \
+	libnvidia-extra-465 \
+	libnvidia-fbc1-465 \
+	libnvidia-gl-465 \
+	libnvidia-ifr1-465 \
+	nvidia-compute-utils-465 \
+	nvidia-dkms-465 \
+	nvidia-driver-465 \
+	nvidia-driver-460 \ 
+	nvidia-kernel-common-465 \ 
+	nvidia-kernel-source-465 \
 	nvidia-settings \
-	nvidia-kernel-source-440 \
-	libnvidia-cfg1-440 \
-	libnvidia-common-440 \
-	libnvidia-compute-440 \
-	libnvidia-decode-440 \
-	libnvidia-encode-440 \
-	libnvidia-extra-440 \
-	libnvidia-fbc1-440 \
-	libnvidia-gl-440 \
-	libnvidia-ifr1-440 \
-	nvidia-compute-utils-440 \
-	nvidia-dkms-440 \
-	nvidia-driver-440 \
-	nvidia-kernel-common-440 \
-	nvidia-settings \
-	nvidia-utils-440 \
-	5.4.0-7642-generic
+	nvidia-utils-465 \
+	5.11.0-7614-generic
 
 # SYSTEM UPDATES
-sudo apt update && sudo apt upgrade -y --allow-downgrades
+sudo apt update
+sudo apt upgrade -y --allow-downgrades
 
 # INSTALL PACKAGES
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg 
-echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list 
 sudo apt update
 sudo apt install -y \
-    codium \
     gtkterm \
     wireguard \
     cmake \
@@ -46,8 +44,7 @@ sudo apt install -y \
     bridge-utils \
     virt-manager \
     backintime-qt \
-    tensorman \
-    nvidia-container-runtime \
+    nvidia-container-toolkit \
     ffmpeg \
     nvidia-cuda-toolkit \
     snapd \
@@ -55,52 +52,73 @@ sudo apt install -y \
     openjdk-11-jdk \
     python3-tk \
     refind \
-    docker-compose \
-    docker.io \
     jq \
-    apt-transport-https \
     gradle \
-    maven 
-#     golang-go
+    maven \
+    golang-go \
+    pypy3 \
+    python3-pip \
+    lm-sensors
     
+# Docker Engine
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+	sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) stable" | \
+	sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt update
+sudo apt install -y \
+	docker-ce \
+	docker-ce-cli \
+	containerd.io \
+	tensorman
+
+# VS Codium
+wget -qO https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg | \
+	gpg --dearmor | \
+	sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg 
+echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | \
+	sudo tee --append /etc/apt/sources.list.d/vscodium.list 
+sudo apt update
+sudo apt install -y codium
+
 sudo usermod -aG libvirt,kvm,tty,docker $USER
 
-mkdir -p $HOME/.local/bin
-echo 'export PATH=$PATH:$HOME/.local/bin' >> .bashrc
+# REMOVE UNWANTED PACKAGES
+sudo apt remove -y \
+	popularity-contest \
+	firefox
 
-# INSTALL APPIMAGES
-# dockstation
-curl -s https://api.github.com/repos/dockstation/dockstation/releases/latest | jq '.assets|map(select(.name|endswith("x86_64.AppImage")))|.[].browser_download_url' | xargs curl -L -o $HOME/.local/bin/dockstation
-chmod +x $HOME/.local/bin/dockstation
+# INSTALL BINARIES/APPIMAGES DIRECTLY
+function installBin(){
+	local NAME="$1"
+	local REPO="$2"
+	local SELECT_NAME="$3"
+	local JQ_FILTER=".assets|map(select(.name|endswith(\"$SELECT_NAME\")))|.[].browser_download_url"
+	local DL_PATH=/usr/local/bin
 
-#todoist
-#curl -L -o $HOME/.local/bin/todoist https://todoist.com/linux_app/snap
-#chmod +x $HOME/.local/bin/todoist
+	curl -s "https://api.github.com/repos/$REPO/releases/latest" | \
+	jq "$JQ_FILTER" | \
+	xargs -I % sudo curl -L % -o "$DL_PATH/$NAME"
+	
+	sudo chmod +x "$DL_PATH/$NAME"
+}
 
-#curl -s https://api.github.com/repos/AppImage/AppImageUpdate/releases | \
-#        python3 -c \
-#        "import sys, json; \
-#        assets = json.load(sys.stdin)[0]['assets']; \
-#        release_url = next((x for x in assets if #x['browser_download_url'].endswith('x86_64.AppImage')), None)['browser_download_url']; \
-#        print(release_url)" | \
-#        xargs -I % sudo curl -L % -o /usr/local/bin/appimageupdate
-#sudo chmod +x /usr/local/bin/appimageupdate
-#curl -s https://api.github.com/repos/jgraph/drawio-desktop/releases/latest | \
-#        python3 -c \
-#        "import sys, json; \
-#        assets = json.load(sys.stdin)['assets']; \
-#        release_url = next((x for x in assets if #x['browser_download_url'].endswith('.AppImage')), None)['browser_download_url']; \
-#        print(release_url)" | \
-#        xargs -I % sudo curl -L % -o /usr/local/bin/drawio
-#sudo chmod +x /usr/local/bin/drawio
+installBin yq mikefarah/yq linux_amd64 
+installBin dockstation dockstation/dockstation x86_64.AppImage
+installBin docker-compose docker/compose Linux-x86_64
+
+#mkdir -p $HOME/.local/bin
+#echo 'export PATH=$PATH:$HOME/.local/bin' >> .bashrc
 
 # INSTALL SNAPS
 sudo snap install kotlin --classic
 sudo snap install flutter --classic
-sudo snap install dotnet-sdk --channel=lts/stable --classic
 sudo snap install google-cloud-sdk --classic
-sudo snap install pypy3 --classic
-sudo snap install yq
 
 # INSTALL FLATPAKS
 flatpak install -y \
@@ -120,47 +138,25 @@ flatpak install -y \
     flathub org.flameshot.Flameshot \
     flathub com.todoist.Todoist \
     flathub com.github.Eloston.UngoogledChromium \
-    io.gitlab.librewolf-community
-
-# INSTALL DOCKER
-# docker engine
-#sudo apt install -y \
-#    apt-transport-https \
-#    ca-certificates \
-#    software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#sudo apt update
-#sudo apt install -y \
-#	docker-ce \
-#	docker-ce-cli \
-#	containerd.io
-#sudo groupadd docker
-#sudo usermod -aG docker $USER
-
-# docker-compose
-#curl -s https://api.github.com/repos/docker/compose/releases/latest | \
-#        python3 -c \
-#        "import sys, json; \
-#        assets = json.load(sys.stdin)['assets']; \
-#        release_url = next((x for x in assets if x['browser_download_url'].endswith('Linux-#x86_64')), None)['browser_download_url']; \
-#        print(release_url)" | \
-#        xargs -I % sudo curl -L % -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
+    io.gitlab.librewolf-community \
+    flathub rest.insomnia.Insomnia \
+    flathub com.axosoft.GitKraken \
+    flathub com.nextcloud.desktopclient.nextcloud
 
 # INSTALL PYTHON, CONDA AND PIP PACKAGES
 #curl https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh | bash
-sudo apt install -y python3-pip
 pip3 install \
     awscli-local \
-    localstack
-# (poetry)
+    localstack \
+    b2 \
+    boto3
+# poetry
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
 export PATH=$PATH:$HOME/.poetry/bin
 
-# NVM & NODE
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-cd ~
+# INSTALL NVM & NODE
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+cd $HOME
 source .bashrc
 nvm install --lts
 nvm use --lts
@@ -177,27 +173,30 @@ chmod +x rustup-init.sh
 # INSTALL DART
 sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
 sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
-sudo apt update && sudo apt install -y dart
+sudo apt update
+sudo apt install -y dart
 dart --disable-analytics
 
-
-# AWS CLI
+# INSTALL AWS CLI
 cd $HOME/Downloads
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 
-# CONFIGURE .NET SDK
-sudo ln -s /snap/dotnet-sdk/current/dotnet /usr/local/bin/dotnet
-sudo snap alias dotnet-sdk.dotnet dotnet
-echo 'export DOTNET_CLI_TELEMETRY_OPTOUT=1' >> ~/.bashrc
-
-
-# STARSHIP
-# nerd font
+# INSTALL .NET SDK
 cd $HOME/Downloads
-mkdir -p $HOME/.fonts
-curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | jq '.assets|map(select(.name == "DejaVuSansMono.zip"))|.[].browser_download_url' | xargs curl -o DejaVuSansMono.zip
+curl -O https://dot.net/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh 
+echo 'export DOTNET_CLI_TELEMETRY_OPTOUT=true' >> ~/.bashrc
+
+# INSTALL STARSHIP
+# nerd fonts
+cd $HOME/Downloads
+git clone https://github.com/ryanoasis/nerd-fonts
+cd nerd-fonts
+chmod +x install.sh
+./install.sh
 # starship
 curl -fsSL https://starship.rs/install.sh | bash
 echo 'eval "$(starship init bash)"' >> $HOME/.bashrc
@@ -206,12 +205,28 @@ mkdir -p $HOME/.config
 # FIX CLOCK
 timedatectl set-local-rtc 1 --adjust-system-clock
 
-# TIMESHIFT-APT
-git clone https://github.com/wmutschl/timeshift-autosnap-apt.git $HOME/Downloads/timeshift-autosnap-apt
-cd $HOME/Downloads/timeshift-autosnap-apt
-sudo make install
-cd ~
+# INSTALL TIMESHIFT-APT
+#git clone https://github.com/wmutschl/timeshift-autosnap-apt.git $HOME/Downloads/timeshift-autosnap-apt
+#cd $HOME/Downloads/timeshift-autosnap-apt
+#sudo make install
+
+# DISABLE USB WAKEUP
+sudo echo'
+[Unit]
+Description=Disables USB mouse/keyboard wakeup
+
+[Service]
+ExecStart=/bin/sh -c "echo XHC0 >> /proc/acpi/wakeup"
+
+[Install]
+WantedBy=multi-user.target
+' > /etc/systemd/system/disable-wakeup.service
+sudo systemctl start disable-wakeup
+sudo systemctl enable disable-wakeup
+
+sudo sensors-detect
 
 # CLEANUP
+cd $HOME
 rm -rf $HOME/Downloads/*
-
+echo "Done"
